749f35c1e8dcb49d1bf321c71e811944814afe34
vamshinoetic <vamshi.gullapelly@wearenoetic.com>
2018-11-11 16:10:56 +0000

    Added implimentation classe for PricingRule
    Added implimentation classe for PricingRule

---
+++ src/main/java/com/kata/shoppingcart/MultiVariationPricingRule.java
@@ -0,0 +1,89 @@
+package com.kata.shoppingcart;
+
+/**
+ * @author vamshikirangullapelly
+ */
+@Order(1)
+public final class MultiVariationPricingRule implements PricingRule {
+
+    public static class MultiItemPricingRuleBuilder {
+
+        private final Item item;
+        private int special;
+
+        public MultiItemPricingRuleBuilder(final Item item) {
+            this.item = item;
+        }
+
+        public MultiItemPricingRuleBuilder buy(final int special) {
+            this.special = special;
+            return this;
+        }
+
+        public MultiVariationPricingRule forPrice(final long price) {
+            return new MultiVariationPricingRule(item, price, special);
+        }
+        
+    }
+
+    public static MultiItemPricingRuleBuilder multiItemPricingRuleFor(final Item item) {
+        return new MultiItemPricingRuleBuilder(item);
+    }
+
+    private final Item item;
+    private final long price;
+    private final int special;
+
+    private MultiVariationPricingRule(final Item item, final long price, final int special) {
+        this.item = item;
+        this.price = price;
+        this.special = special;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public Unit apply(final long amount) {
+        final long applies = amount / special;
+        final long totalPrice = price * applies;
+        return new Unit(totalPrice, applies * special);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((item == null) ? 0 : item.hashCode());
+        result = prime * result + (int) (price ^ (price >>> 32));
+        result = prime * result + special;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MultiVariationPricingRule other = (MultiVariationPricingRule) obj;
+        if (item == null) {
+            if (other.item != null)
+                return false;
+        } else if (!item.equals(other.item))
+            return false;
+        if (price != other.price)
+            return false;
+        if (special != other.special)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "item " + item + " at " + price + "p for " + special;
+    }
+
+}
